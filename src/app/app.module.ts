203ffb0785051bc72db43461749bd3a9b9017f45
Xurmein <wj.pavocrist@gmail.com>
2018-12-06 19:16:23 -0500

    restructuring commit|combined work of AH, GA, JP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AboutComponent } from './about/about.component';
+import { MissionComponent } from './mission/mission.component';
+import { ApiSearchComponent } from './protectedViews/api-search/api-search.component';
+import { JournalComponent } from './protectedViews/journal/journal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    UserProfileComponent,
+    AboutComponent,
+    MissionComponent,
+    ApiSearchComponent,
+    JournalComponent
   ],
   imports: [
     BrowserModule,
