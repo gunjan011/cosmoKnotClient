e19fa4e128a7d704363b1b80081ce6d199ff48f4
Xurmein <wj.pavocrist@gmail.com>
2018-12-16 01:09:08 -0500

    building admin-api-search & service

--- src/app/protectedViews/api-search/api-search.component.ts
+++ src/app/protectedViews/api-search/api-search.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiSearchService, base } from '../../services/api-search.service'
 
 @Component({
   selector: 'app-api-search',
   templateUrl: './api-search.component.html',
   styleUrls: ['./api-search.component.css']
 })
-export class ApiSearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ApiSearchComponent {
+  public endpoint : Endpoint = {
+    baseURL : base,
+    termURL : '',
+    typeURL : ''
   }
 
+  results : Results[]; 
+
+  constructor(
+    private apiSearchService : ApiSearchService
+  ) { }
+
+  submitSearch() : any {
+    this.apiSearchService.geneLabSearch(this.endpoint.termURL, this.endpoint.typeURL)
+      .subscribe(results => this.results = results)
+  }
+
+}
+export interface Endpoint {
+  baseURL : string;
+  termURL : string;
+  typeURL : string
+}
+
+export interface Results {
+  results : any
 }
