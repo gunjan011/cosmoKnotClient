720e8214debca2290c4f4059f02588a86bd32a08
Gunjan Aulakh <gunjan@Gunjans-MacBook-Pro.local>
2018-12-18 21:22:30 -0500

    trial and error?

--- src/app/services/apod.service.ts
+++ src/app/services/apod.service.ts
@@ -13,7 +13,8 @@ import { ApodResponse } from '../models/apod.response';
   providedIn: 'root'
 })
 export class ApodService {
-  // nasa_api_key1: string = gkey.nasa_api_key1;
+  nasa_api_key1: string = '';
+  //  nasa_api_key1: string = gkey.nasa_api_key1;
    nasa_url =  `https://api.nasa.gov/planetary/apod?api_key=${this.nasa_api_key1}&date&count=1`;
   
   constructor(private httpClient: HttpClient) { }
